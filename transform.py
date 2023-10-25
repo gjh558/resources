d9db1a20b2de8454239a7ecedf6da4132799c917
gjh558 <gjh558@126.com>
2023-10-25 23:08:57 +0800

    update

--- transform.py
+++ transform.py
@@ -2,11 +2,15 @@
 
 import json
 import sys
+import zipfile
+import gzip
+import requests
+import xml.etree.ElementTree as ET
 
 class Trans:
     def __init__(self, pos, trans):
         self.pos = pos
-        self.trans = trans
+        self.ex = trans
 
 class Sentence:
     def __init__(self, en, cn):
@@ -14,12 +18,11 @@ class Sentence:
         self.cn = cn
 
 class Word:
-    def __init__(self, headWord, usphone, ukphone, trans, sentence):
-        self.name = headWord.lower()
-        self.usphone = usphone
-        self.ukphone = ukphone
-        self.trans = trans
-        self.sentence = sentence
+    def __init__(self, headWord, phones, trans, sentence):
+        self.n = headWord.lower()
+        self.t = trans
+        self.s = sentence
+        self.p = phones
 
 def replaceFran(str):
     fr_en = [['é', 'e'], ['ê', 'e'], ['è', 'e'], ['ë', 'e'], ['à', 'a'], ['â', 'a'], ['ç', 'c'], ['î', 'i'], ['ï', 'i'],
@@ -28,6 +31,32 @@ def replaceFran(str):
     for i in fr_en:
         str = str.replace(i[0], i[1])
     return str
+
+def parseXML(strXML):
+    root = ET.fromstring(strXML)
+    keyTag = root.find('key')
+    psTag = root.findall('ps')
+    phones = [psTag[1].text, psTag[0].text]
+    posTag = root.findall('pos')
+    acceptationTag = root.findall('acceptation')
+    trans = []
+    for i in range(len(posTag)):
+        trans.append(Trans(posTag[i].text, acceptationTag[i].text))
+    sentTag = root.findall('sent')
+    maxSentNum = min(len(sentTag), 2)
+    sents = []
+    for s in sentTag:
+        sents.append(Sentence(s[0].text, s[1].text))
+
+    wordObj = Word(keyTag.text, phones, trans, sents)
+    print(json.dumps(wordObj, default=vars))
+
+def queryWord(word):
+    r = requests.get('https://dict-co.iciba.com/api/dictionary.php?key=C8DDDA9B1360645BA7C3888DF6F54702&w=' + word.lower())
+    if r.status_code != 200:
+        print("ERROR: failed to query: " + word)
+        return None
+    return parseXML(r.text)
 
 def transform(path):
     output = []
@@ -41,35 +70,58 @@ def transform(path):
             t = []
             src_trans = src['trans']
             for tr in src_trans:
+                if 'pos' not in tr.keys() or 'tranCn' not in tr.keys():
+                    continue
                 t.append(Trans(tr['pos'], tr['tranCn']))
-            if 'sentence' not in src.keys():
-                s_error+=1
-            if 'usphone' not in src.keys() or 'ukphone' not in src.keys():
-                p_error+=1
 
-            s = Sentence(src['sentence']['sentences'][0]['sContent'], src['sentence']['sentences'][0]['sCn']) if 'sentence' in src.keys() and len(src['sentence']['sentences']) > 0 else Sentence('', '')
-            usphone, ukphone = '', ''
-            if 'usphone' in src.keys():
-                usphse = src['usphone']
-            if 'ukphone' in src.keys():
-                ukphse = src['ukphone']
+            sents = []
+            if 'sentence' in src.keys():
+                if 'sentences' in src['sentence'].keys():
+                    for sent in src['sentence']['sentences']:
+                        sents.append(Sentence(sent['sContent'], sent['sCn']))
 
-            w = Word(word, usphone, ukphone, t, s)
-            output.append(w)
+            phones = []
+            if 'usphone' in src.keys() and 'ukphone' in src.keys():
+                phones.append(src['usphone'])
+                phones.append(src['ukphone'])
+            # validation
+            if len(t) == 0 or len(sents) == 0 or len(phones) != 2:
+                print('try to query: ' + word)
+                res = queryWord(word)
+                if res == None:
+                    continue
+                output.append(res)
+            else:
+                w = Word(word, phones, t, sents)
+                output.append(w)
 
-    print(s_error, p_error)
     return output
 
 def sorter(e):
-    return e.name
+    return e.n
 
 if len(sys.argv) != 2:
     print('wrong args')
     exit()
 
-res = transform(sys.argv[1])
+# unzip
+zfile = zipfile.ZipFile(sys.argv[1] + '/word.zip','r')
+if len(zfile.namelist()) != 1:
+    print(sys.argv[1])
+    exit()
+
+for filename in zfile.namelist():
+    data = zfile.read(filename)
+    file = open(sys.argv[1] + '/word.txt', 'w+b')
+    file.write(data)
+    file.close()
+
+
+res = transform(sys.argv[1] + '/word.txt')
 res.sort(key=sorter)
 
-f = open('words.js', 'w+')
-f.write(json.dumps(res, default=vars))
+
+with open(sys.argv[1] + '.gz', 'w+b') as f:
+    f.write(gzip.compress(str.encode(json.dumps(res, default=vars))))
+
 f.close()
